042277b870cb70d4b0f5d1118af5b202aa35a9f4
Dmitriy Elisov <dmitriy.elisov@aurea.com>
2022-11-27 11:16:34 +0300

    Added maybe_unused to INetwork

--- BrightPattern/INetwork.h
+++ BrightPattern/INetwork.h
@@ -5,7 +5,7 @@
 class INetwork {
 public:
 	virtual void addActivity(NetworkActivity) = 0;
-	virtual std::list<NetworkActivity> Select(unsigned) = 0;
+	[[maybe_unused]] virtual std::list<NetworkActivity> Select(unsigned) = 0;
 	virtual void sendReply(int, std::shared_ptr<Reply>) = 0;
 	virtual bool shouldExit() const = 0;
 };
